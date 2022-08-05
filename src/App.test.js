57773fd68e5a757026cc8dfebf562f5f034eb62c
Saranya Eswaralal <saranyaeswaralal@Saranyas-MacBook-Pro.local>
2022-08-05 15:20:01 +0530

    Add. Test method

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders default view in screen', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByRole("heading");
+  expect(title).toBeInTheDocument();
+  expect(title).toHaveTextContent("Menu List");
+
+  expect(screen.getByRole('button')).toHaveTextContent("Add Menu");
+  expect(screen.getByRole('table')).toBeInTheDocument();
 });
